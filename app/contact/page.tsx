73362ac0b2df09485db9bc4f21d29dd1a20953a4
devwalshe <wwalsh0615@gmail.com>
2023-07-12 16:30:27 -0400

    updating portfolio to vercel

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/Walshesway",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@Walshesway",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:Walshedev@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "walshedev@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/devwalshe",
 		label: "Github",
-		handle: "chronark",
+		handle: "devwalshe",
 	},
 ];
 
